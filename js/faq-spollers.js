0836bfa341edfb689d5c8e8e8bb94cfd577c86cf
jullis2003 <lisunjulia@gmail.com>
2024-05-09 16:45:29 +0300

    correct accordion

--- js/faq-spollers.js
+++ js/faq-spollers.js
@@ -1,40 +1,25 @@
-const accordion = document.querySelector('.accordion');
-const panels = document.querySelectorAll('.faq-spoller');
+const spollerTitles = document.querySelectorAll('.spoller-question');
 
-accordion.addEventListener('click', e => {
-  const activePanel = e.target.closest('.faq-spoller');
-
-  // panels.forEach(item => {
-  //   const button = item.querySelector('.spoller-question');
-  //   const panel = item.querySelector('.accordion-content');
-
-  //   item.classList.remove('active');
-  //   button.classList.remove('active');
-  //   button.setAttribute('aria-expanded', true);
-  //   panel.setAttribute('aria-hidden', false);
-  // });
-
-  if (!activePanel) return;
-
-  toggleAccordion(activePanel);
+spollerTitles.forEach(element => {
+  element.addEventListener('click', accordionHandler);
 });
 
-function toggleAccordion(panelToActivate) {
-  const activeButton = panelToActivate.querySelector('.spoller-question');
-  const activePanel = panelToActivate.querySelector('.accordion-content');
-  const activePanelIsOpened = activeButton.getAttribute('aria-expanded');
+function accordionHandler(event) {
+  event.preventDefault();
 
-  if (activePanelIsOpened === 'true') {
-    activeButton.setAttribute('aria-expanded', false);
-    activePanel.setAttribute('aria-hidden', true);
+  const spoller = event.target.closest('.faq-spoller');
+  const spollerContent = event.target.nextElementSibling;
 
-    panelToActivate.classList.add('active');
-    activeButton.classList.add('active');
+  if (spoller.classList.contains('active')) {
+    spoller.classList.remove('active');
+    spollerContent.style.maxHeight = '0px';
   } else {
-    activeButton.setAttribute('aria-expanded', true);
-    activePanel.setAttribute('aria-hidden', false);
+    document.querySelectorAll('.faq-spoller').forEach(element => {
+      element.classList.remove('active');
+      element.querySelector('.spoller-content').style.maxHeight = '0px';
+    });
 
-    panelToActivate.classList.remove('active');
-    activeButton.classList.remove('active');
+    spoller.classList.add('active');
+    spollerContent.style.maxHeight = spollerContent.scrollHeight + 'px';
   }
 }
